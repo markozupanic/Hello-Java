25e394cd110fa8b85334f8d4758a5fcf3fbc3cec
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-06-10 17:37:49 +0200

    predavanja

---
+++ primitivnitipovipodataka.java
@@ -0,0 +1,32 @@
+public class E02PrimitivniTipoviPodataka {
+
+	public static void main(String[] args) {
+		
+		//https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+		
+		//logička vrijesnost
+		boolean aktivan = true;
+		//cijeli brojevi
+		byte maliBroj = -25;
+		short maloVeciBroj = 589;
+		int integer = 2656; //ovo ćemo koristiti
+		long velikiBroj = 33333;
+		//decimalni brojevi
+		float manjiDecimalni = 3.14f;
+		double velikiDecimalniBroj = 3.14;
+		//znakovni
+		char znak = 't';
+		
+		// ovako nešto ne postoji
+		//integer.parse("");
+		
+		
+		
+		
+	}
+}
+
+
+
+
+
